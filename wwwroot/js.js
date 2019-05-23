48e83c59a4cd141ce2a25364d39a0f0d33be262e
Samu Lang <langsamu@hotmail.com>
2019-05-23 22:06:44 +0100

    More JS rigour.

--- wwwroot/js.js
+++ wwwroot/js.js
@@ -2,27 +2,23 @@
     "use strict";
 
     function getSvgElement(id) {
-        var svg = e;
+        const svg = document.getElementById("e");
         return svg.getElementById(id);
     }
     function tokenize(input) {
-        var element = document.createElement("div");
+        const element = document.createElement("div");
         element.className = input;
         return element.classList.toArray();
     }
     function onResetClick() {
-        var forItem = document.getElementById(this.dataset["for"]);
+        const forItem = document.getElementById(this.dataset["for"]);
         forItem.value = forItem.defaultValue;
         forItem.update();
     }
     function enumerate(input, callback) {
-        var svgElementIds = tokenize(input.dataset.for);
-
-        svgElementIds
+        tokenize(input.dataset.for)
             .map(getSvgElement)
-            .forEach(function (svgElement) {
-                callback.call(input, svgElement);
-            });
+            .forEach(callback.bind(input));
     }
     function getPathNavigatorReduceFunction(value) {
         return function (previousValue, currentValue, index, array) {
@@ -35,8 +31,8 @@
     }
 
     async function processInputs() {
-        var response = await fetch("inputs.json");
-        var inputs = await response.json();
+        const response = await fetch("inputs.json");
+        const inputs = await response.json();
 
         inputs.forEach(function (input) {
             processNode(input, fieldsets);
@@ -46,23 +42,23 @@
         attachResetButtons();
     }
     function processNode(node, context) {
-        var processingFunctions = {
+        const processingFunctions = {
             fieldset: processFieldset,
             input: processInput
         };
 
-        var processingFunction = processingFunctions[node.nodeType];
+        const processingFunction = processingFunctions[node.nodeType];
         if (processingFunction) {
             processingFunction.call(this, node, context);
         }
     }
     function processFieldset(node, context) {
-        var detailsElement = document.createElement("details");
+        const detailsElement = document.createElement("details");
         context.appendChild(detailsElement);
 
         detailsElement.id = generateId(detailsElement, context);
 
-        var summaryElement = document.createElement("summary");
+        const summaryElement = document.createElement("summary");
         detailsElement.appendChild(summaryElement);
 
         summaryElement.innerText = node.legend;
@@ -72,20 +68,20 @@
         });
     }
     function processInput(node, context) {
-        var divElement = document.createElement("div");
+        const divElement = document.createElement("div");
         context.appendChild(divElement);
 
-        var id = generateId(divElement, context);
+        const id = generateId(divElement, context);
 
         divElement.title = node.title;
 
-        var label = document.createElement("label");
+        const label = document.createElement("label");
         divElement.appendChild(label);
 
         label.htmlFor = id;
         label.innerText = node.label;
 
-        var inputElement = document.createElement("input");
+        const inputElement = document.createElement("input");
         divElement.appendChild(inputElement);
 
         inputElement.id = id;
@@ -113,13 +109,13 @@
         inputElement.dataset.pointIndex = node.pointIndex;
         inputElement.dataset.valueIndex = node.valueIndex;
 
-        var outputElement = document.createElement("output");
+        const outputElement = document.createElement("output");
         divElement.appendChild(outputElement);
 
         outputElement.setAttribute("for", id);
 
         if (node.inputType !== "button") {
-            var buttonElement = document.createElement("button");
+            const buttonElement = document.createElement("button");
             divElement.appendChild(buttonElement);
 
             buttonElement.dataset.type = "reset";
@@ -133,8 +129,8 @@
 
     function attachListeners() {
         document.querySelectorAll("[data-type]").toArray().forEach(function (item) {
-            var listenerType = item.dataset.type;
-            var listener = listeners[listenerType];
+            const listenerType = item.dataset.type;
+            const listener = listeners[listenerType];
 
             switch (item.type) {
                 case "button":
@@ -156,15 +152,15 @@
         });
     }
 
-    var listeners = {
+    const listeners = {
         saveSvg: function () {
-            var svg = e;
-            var xml = svg.outerHTML;
+            const svg = document.getElementById("e");
+            const xml = svg.outerHTML;
 
-            var svgBlob = new Blob([xml], { type: "image/svg+xml; charset=utf-8" });
-            var url = URL.createObjectURL(svgBlob);
+            const svgBlob = new Blob([xml], { type: "image/svg+xml; charset=utf-8" });
+            const url = URL.createObjectURL(svgBlob);
 
-            var link = document.createElement("a");
+            const link = document.createElement("a");
             link.download = "pose.svg";
             link.href = url;
             link.click();
@@ -172,24 +168,24 @@
             URL.revokeObjectURL(svgBlob);
         },
         savePng: function () {
-            var svg = e;
-            var xml = svg.outerHTML;
+            const svg = document.getElementById("e");
+            const xml = svg.outerHTML;
 
-            var image = new Image(e.clientWidth, e.clientHeight);
+            const image = new Image(e.clientWidth, e.clientHeight);
 
-            var svgBlob = new Blob([xml], { type: "image/svg+xml;charset=utf-8" });
-            var url = URL.createObjectURL(svgBlob);
+            const svgBlob = new Blob([xml], { type: "image/svg+xml;charset=utf-8" });
+            const url = URL.createObjectURL(svgBlob);
 
             image.addEventListener("load", function () {
-                var canvas = document.createElement("canvas");
+                const canvas = document.createElement("canvas");
                 canvas.setAttribute("width", e.clientWidth);
                 canvas.setAttribute("height", e.clientHeight);
 
-                var context = canvas.getContext("2d");
+                const context = canvas.getContext("2d");
 
                 context.drawImage(image, 0, 0);
 
-                var link = document.createElement("a");
+                const link = document.createElement("a");
                 link.download = "pose.png";
                 link.href = canvas.toDataURL();
                 link.click();
@@ -201,14 +197,14 @@
         },
         pathData: function () {
             enumerate(this, function (svgElement) {
-                var pathData = svgElement.getPathData();
+                const pathData = svgElement.getPathData();
                 pathData[this.dataset.pointIndex].values[this.dataset.valueIndex] = this.value;
                 svgElement.setPathData(pathData);
             });
         },
         propertyPath: function () {
-            var properties = tokenize(this.dataset.path);
-            var boundStepper = getPathNavigatorReduceFunction(this.value);
+            const properties = tokenize(this.dataset.path);
+            const boundStepper = getPathNavigatorReduceFunction(this.value);
 
             enumerate(this, function (svgElement) {
                 properties.reduce(boundStepper, svgElement);
@@ -216,45 +212,45 @@
         },
         rotate: function () {
             enumerate(this, function (svgElement) {
-                var transformIndex = this.dataset.transformIndex;
+                const transformIndex = this.dataset.transformIndex;
 
                 svgElement.transform.baseVal[transformIndex].setRotate(this.value, 0, 0);
             });
         },
         scaleX: function () {
             enumerate(this, function (svgElement) {
-                var transform = svgElement.transform.baseVal[2];
+                const transform = svgElement.transform.baseVal[2];
                 transform.setScale(this.value, transform.matrix.d);
             });
         },
         scaleY: function () {
             enumerate(this, function (svgElement) {
-                var transform = svgElement.transform.baseVal[2];
+                const transform = svgElement.transform.baseVal[2];
                 transform.setScale(transform.matrix.a, this.value);
             });
         },
         distance: function () {
-            var svgElements = tokenize(this.dataset.for)
+            const svgElements = tokenize(this.dataset.for)
                 .map(getSvgElement);
 
             svgElements[0].transform.baseVal[0].matrix.e = -this.valueAsNumber;
             svgElements[1].transform.baseVal[0].matrix.e = this.valueAsNumber;
         },
         rotateMirrored: function () {
-            var svgElements = tokenize(this.dataset.for)
+            const svgElements = tokenize(this.dataset.for)
                 .map(getSvgElement);
 
             svgElements[0].transform.baseVal[1].setRotate(this.valueAsNumber, 0, 0);
             svgElements[1].transform.baseVal[1].setRotate(-this.valueAsNumber, 0, 0);
         },
         hairQuantity: function () {
-            var element = e.getElementById("hair");
+            const element = e.getElementById("hair");
             element.setAttribute("data-quantity", this.valueAsNumber);
 
             recreateHair();
         },
         hairDistance: function () {
-            var element = e.getElementById("hair");
+            const element = e.getElementById("hair");
             element.setAttribute("data-distance", this.valueAsNumber);
 
             recreateHair();
@@ -267,26 +263,26 @@
     };
 
     function recreateHair() {
-        var svg = e;
-        var hair = svg.getElementById("hair");
-        var head = svg.getElementById("head");
-        var distance = parseInt(hair.getAttribute("data-distance"));
-        var quantity = parseInt(hair.getAttribute("data-quantity"));
+        const svg = document.getElementById("e");
+        const hair = svg.getElementById("hair");
+        const head = svg.getElementById("head");
+        const distance = parseInt(hair.getAttribute("data-distance"));
+        const quantity = parseInt(hair.getAttribute("data-quantity"));
 
-        var uses = hair.querySelectorAll("use");
-        for (var i = uses.length - 1; i > -1; i--) {
+        const uses = hair.querySelectorAll("use");
+        for (let i = uses.length - 1; i > -1; i--) {
             hair.removeChild(uses[i]);
         }
 
-        var rotationAmount = (quantity - 1) * distance / -2;
-        var a = head.ry.baseVal.value;
-        var b = head.rx.baseVal.value;
+        let rotationAmount = (quantity - 1) * distance / -2;
+        const a = head.ry.baseVal.value;
+        const b = head.rx.baseVal.value;
 
-        for (var j = 0; j < quantity; j++) {
-            var rot = rotationAmount.toRadians();
-            var translateAmount = -a * b / Math.sqrt(Math.pow(b * Math.cos(rot), 2) + Math.pow(a * Math.sin(rot), 2));
+        for (let j = 0; j < quantity; j++) {
+            const rot = rotationAmount.toRadians();
+            const translateAmount = -a * b / Math.sqrt(Math.pow(b * Math.cos(rot), 2) + Math.pow(a * Math.sin(rot), 2));
 
-            var use = document.createElementNS("http://www.w3.org/2000/svg", "use");
+            const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
             use.setAttribute("transform", "rotate(" + rotationAmount + ") translate(0, " + translateAmount + ")");
             use.setAttributeNS("http://www.w3.org/1999/xlink", "href", "#hairStrand");
             hair.appendChild(use);
